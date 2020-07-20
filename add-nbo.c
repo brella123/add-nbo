2bdf56f40dee9ee554a539fa0ac545d0af9f8f75
brella123 <brella123@naver.com>
2020-07-19 23:36:29 -0700

    Fixed add-nbo.c

--- add-nbo.c
+++ add-nbo.c
@@ -7,8 +7,8 @@ int main(int argc, char* argv[])
 {
 	uint32_t a, b;
 
-	FILE *fp_a = fopne(argv[1],'rb');
-	FILE *fp_b = fopen(argv[2],'rb');
+	FILE *fp_a = fopen(argv[1],"rb");
+	FILE *fp_b = fopen(argv[2],"rb");
 	
 	fread(&a, sizeof(a), 1, fp_a);
 	fread(&b, sizeof(b), 1, fp_b);
@@ -18,8 +18,8 @@ int main(int argc, char* argv[])
 
 	printf("%d(%x) + %d(%x) = %d(%x)\n", a, a, b, b, a+b, a+b);
 
-	fclose(a);
-	fclose(b);
+	fclose(fp_a);
+	fclose(fp_b);
 
 	return 0;
 }
